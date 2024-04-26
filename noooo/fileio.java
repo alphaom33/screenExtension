8967d80571216dc1a10baa457a0e97080de08130
Matthew Borchert <alphaom34@gmail.com>
2024-04-26 14:29:53 -0500

    javaed

--- noooo/fileio.java
+++ noooo/fileio.java
@@ -2,10 +2,22 @@ import java.io.*;
 
 class fileio {
     public static void main(String[] args) {
+        writeGradient("visual.txt", "|", true);
+        writeGradient("yes.txt", "", false);
+    }
+
+    private static void writeGradient(String name, String add, boolean line) {
         try {
-            FileWriter a = new FileWriter(new File("data/yes.txt"));
-            for (int i = 0; i < 300 * 200; i++) {
-                a.write("00000000\n");
+            FileWriter a = new FileWriter(new File("data/" + name));
+            for (int i = 0; i < 300; i++) {
+                for (int j = 0; j < 200; j++) {
+                    String g = Integer.toHexString((int) ((double) i * (256.0 / 300.0)));
+                    g += g.length() == 1 ? "0" : "";
+                    String b = Integer.toHexString((int) ((double) j * (256.0 / 200.0)));
+                    b += b.length() == 1 ? "0" : "";
+                    a.write("00" + g + b + add);
+                }
+                if (line) a.write("\n");
             }
             a.close();
         } catch (IOException e) {
