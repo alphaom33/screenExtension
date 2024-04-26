733983fed84e95ee492a1951f79dea06130228a4
Matthew Borchert <alphaom34@gmail.com>
2024-04-26 14:44:53 -0500

    cleaning

--- noooo/src/help.js
+++ noooo/src/help.js
@@ -31,36 +31,6 @@ try {
             }
         }
     }, 0);
-
-    // setInterval(() => {
-    //     debug.textContent = "laskdfj";
-    //     if (toWrite !== "") {
-    //         for (let i = 0; i < 300; i++) {
-    //             for (let k = 0; j < 200; i++) {
-    //                 context.fillRect(i, j, 1, 1);
-    //             }
-    //         }
-    //     }
-    // }, 0);
-
-
-    // let count = 0;
-    // setInterval(() => {
-    //     counter.textContent = count++;
-    // }, 100);
-
-    // Handle the message inside the webview
-    // window.addEventListener('message', event => {
-
-    //     const message = event.data; // The JSON data our extension sent
-
-    //     switch (message.command) {
-    //         case 'refactor':
-    //             count = Math.ceil(count * 0.5);
-    //             counter.textContent = count;
-    //             break;
-    //     }
-    // });
 } catch (error) {
     debug.textContent = error;
 }
